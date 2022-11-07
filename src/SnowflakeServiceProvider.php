ea422e9be89ed83016194c39e0958741821ece21
Jakye <jakyeru@gmail.com>
2022-11-07 22:07:50 +0200

    Added SnowflakeServiceProvider.php

---
+++ src/SnowflakeServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Jakyeru\snowflake;
+
+use Godruoyi\Snowflake\LaravelSequenceResolver;
+use Godruoyi\Snowflake\Snowflake;
+use Illuminate\Support\ServiceProvider;
+
+class SnowflakeServiceProvider extends ServiceProvider {
+    public function register()
+    {
+        $this->app->singleton('snowflake', function ($app) {
+            return (new Snowflake(config('snowflake.data_center', 1), config('snowflake.worker_node', 1)))
+                ->setStartTimeStamp(strtotime('2022-01-01') * 1000)
+                ->setSequenceResolver(new LaravelSequenceResolver($app->get('cache.store')));
+        });
+    }
+
+    public function boot()
+    {
+
+    }
+
+    protected function registerConfiguration()
+    {
+        $this->publishes([
+            __DIR__ . '/config/snowflake.php' => config_path('snowflake.php')
+        ], 'config');
+    }
+}
