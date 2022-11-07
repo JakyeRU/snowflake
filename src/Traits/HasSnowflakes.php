bec6fcd0439ff20ff4a0881c49f66be2655c9801
Jakye <jakyeru@gmail.com>
2022-11-07 22:08:02 +0200

    Added HasSnowflakes.php Trait

---
+++ src/Traits/HasSnowflakes.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Jakyeru\snowflake\Traits;
+
+trait HasSnowflakes
+{
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function($model) {
+            if (!$model->getKey()) {
+                $key = resolve('snowflake')->id();
+                $model->{$model->getKeyName()} = $key;
+            }
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+}
