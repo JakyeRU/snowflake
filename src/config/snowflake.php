fc9edce7258b13e81419b6e98fc6d8452d513244
Jakye <jakyeru@gmail.com>
2022-11-08 14:05:23 +0200

    Updated snowflake.php & SnowflakeServiceProvider.php

--- src/config/snowflake.php
+++ src/config/snowflake.php
@@ -6,16 +6,35 @@ return [
     |--------------------------------------------------------------------------
     | Data Center
     |--------------------------------------------------------------------------
+    |
+    | This value represents the data center that will be used
+    | when generating snowflakes.
+    |
     */
 
-    'data_center' => env('SNOWFLAKE_DATA_CENTER', 1),
+    'data_center' => 1,
 
     /*
     |--------------------------------------------------------------------------
     | Worker Node
     |--------------------------------------------------------------------------
+    |
+    | This value represents the worker node that will be used
+    | when generating snowflakes.
+    |
     */
 
-    'worker_node' => env('SNOWFLAKE_WORKER', 1),
+    'worker_node' => 1,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Start Date
+    |--------------------------------------------------------------------------
+    |
+    | This value represents the starting date for generating new timestamps.
+    | This should be set to the current date when creating a new application.
+    |
+    */
+
+    'start_date' => '2022-01-01',
 ];
